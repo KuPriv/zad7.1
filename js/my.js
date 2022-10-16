245271dcaf2d44429709b356bbd116fecaa551fa
KuPriv <113209570+KuPriv@users.noreply.github.com>
2022-10-16 16:06:59 +0300

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".content").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
